0e9d716a9f64ae6c9125a90a62b043746b82dc0f
MarceloBill84 <marcelobill@msn.com>
2021-08-22 12:56:14 -0300

    implementing report

--- boasaudesite/src/components/Provider/Reports/AttendanceXManifestation/attendanceXManifestation.js
+++ boasaudesite/src/components/Provider/Reports/AttendanceXManifestation/attendanceXManifestation.js
@@ -15,27 +15,6 @@ const notifyError = (message) => {
     toast.error(message, { position: toast.POSITION.TOP_CENTER, autoClose: 4000 })
 }
 
-const data = {
-    labels: ['1', '2', '3', '4', '5', '6'],
-    datasets: [
-        {
-            label: '# Manifestações',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: 'rgb(255, 99, 132)',
-        },
-        {
-            label: '# Atendimentos',
-            data: [2, 3, 20, 5, 1, 4],
-            backgroundColor: 'rgb(54, 162, 235)',
-        },
-        {
-            label: '# of Green Votes',
-            data: [3, 10, 13, 15, 22, 30],
-            backgroundColor: 'rgb(75, 192, 192)',
-        },
-    ],
-};
-
 const options = {
     scales: {
         yAxes: [
@@ -73,62 +52,36 @@ async function getReportData(token) {
 
 export default function AttendanceXManifestation() {
     const { token } = useToken();
-    const [chartData, setChartData] = useState({});
+    const [chartData, setChartData] = useState(null);
 
-    getReportData(token).then(p => {
-        console.log(p);
-        /*setChartData({
-            labels: p.months,
-            datasets: [
-                {
-                    label: '# Manifestações',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: 'rgb(255, 99, 132)',
-                },
-                {
-                    label: '# Atendimentos',
-                    data: [2, 3, 20, 5, 1, 4],
-                    backgroundColor: 'rgb(54, 162, 235)',
-                },
-                {
-                    label: '# of Green Votes',
-                    data: [3, 10, 13, 15, 22, 30],
-                    backgroundColor: 'rgb(75, 192, 192)',
-                },
-            ],
-        });*/
-    });
-    
-    /*setChartData({
-        labels: teste.months,
-        datasets: [
-            {
-                label: '# Manifestações',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: 'rgb(255, 99, 132)',
-            },
-            {
-                label: '# Atendimentos',
-                data: [2, 3, 20, 5, 1, 4],
-                backgroundColor: 'rgb(54, 162, 235)',
-            },
-            {
-                label: '# of Green Votes',
-                data: [3, 10, 13, 15, 22, 30],
-                backgroundColor: 'rgb(75, 192, 192)',
-            },
-        ],
-    });*/
-
+    if (chartData === null) {
+        getReportData(token).then(p => {
+            setChartData({
+                labels: p.months,
+                datasets: [
+                    {
+                        label: 'Atendimentos',
+                        data: p.attendances,
+                        backgroundColor: 'rgb(54, 162, 235)',
+                    },
+                    {
+                        label: 'Manifestações',
+                        data: p.manifestations,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                    }
+                ],
+            });
+        });
+    }
 
     return (
         <div>
             <Navbar />
             <div className="container" >
-                <div className='header'>
-                    <h1 className='title'>Relatório de Atendimentos x Manifestações</h1>
+                <div className='text-center p-5'>
+                    <h2 >Relatório de Atendimentos x Manifestações</h2>
                 </div>
-                <Bar data={data} options={options} />
+                <Bar data={chartData} options={options} />
             </div>
         </div>
     )
